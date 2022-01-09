5726bcd3bea445241dd1d3b1f5908279f2235595
tiago <tiago.matos@coreform.co>
2022-01-09 21:08:55 +1100

    Refactored GetTasksFilterDto added class-validator

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TASK_STATUS_ENUM } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TASK_STATUS_ENUM)
   status?: TASK_STATUS_ENUM;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
