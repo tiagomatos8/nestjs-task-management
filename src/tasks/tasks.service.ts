af13b6c6ec46778855bc8aa578a587c1f9135556
tiago <tiago.matos@coreform.co>
2022-01-09 18:24:50 +1100

    Refactored getTaskById service to throw NotFoundException

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TASK_STATUS_ENUM } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -43,7 +43,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException(`Task ${id} not found`);
+    }
+    return task;
   }
 
   deleteTask(id: string): void {
