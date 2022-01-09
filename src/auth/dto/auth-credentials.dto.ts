edc158bdc7da14cc7a47d47f9acfdb541271c477
tiago <tcm.matos@gmail.com>
2022-01-10 02:23:52 +1100

    Added validations to AuthCredentialsDto

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: `At least 1 upper case letter AND 1 lower case letter AND 1 number or special character`,
+  })
   password: string;
 }
